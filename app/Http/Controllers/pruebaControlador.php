dfba4d69f727264b74f56fcc342e959842429f62
Julio Vásquez Díaz <ingjuliovasquez@gmail.com>
2018-03-04 18:26:23 -0600

    Initial commit

---
+++ app/Http/Controllers/pruebaControlador.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pruebaControlador extends Controller
+{
+    
+
+public function welcome()
+{
+	return view('welcome');
+	
+}
+
+
+}
