7a40a83d2a2c86bbb487417d96fa9a437639665a
Julio Vásquez Díaz <ingjuliovasquez@gmail.com>
2018-03-05 08:33:06 -0600

    Cambios1
    Nuevos Cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    JULIO
                 </div>
 
                 <div class="links">
